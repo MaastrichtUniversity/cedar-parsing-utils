507196fcd85c82d36472828c1246a374b1d070aa
JonathanMELIUS <meliusjonathan@gmail.com>
2022-03-02 14:03:18 +0100

    Move parsing logic to this repo [DHS-1821]

---
+++ cedarparsingutils/utils.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class CedarSchemaName(Enum):
+    """This class enumerates all currently existing Cedar field types (=schema name)"""
+    TEXTFIELD = 1
+    TEMPORAL = 2
+    EMAIL = 3
+    NUMERIC = 4
+    LINK = 5
+    TEXTAREA = 6
+    RADIO = 7
+    CHECKBOX = 8
+    PAGE_BREAK = 9
+    SECTION_BREAK = 10
+    # Not supported (yet)
+    LIST = 11
+    PHONE_NUMBER = 12
+    ATTRIBUTE_VALUE = 13
